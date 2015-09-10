c8bc2aee0690c571919cbb2782c4567047ebf0e9
Greg Rahn <gregrahn@users.noreply.github.com>
2015-09-10 16:54:57 -0700

    add Apache License changes

--- tests/testthat/test-sql-translation.R
+++ tests/testthat/test-sql-translation.R
@@ -1,5 +1,5 @@
 # Copyright 2015 Snowflake Computing Inc.
-# Copyright 2013-2015 RStudio
+# (derived from dplyr, Copyright 2013-2015 RStudio)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
